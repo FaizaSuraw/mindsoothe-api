cbdda0f0eb375fd4dbf952072d7d1834514a0488
FaizaSuraw <itssumeya@gmail.com>
2025-08-24 09:25:51 +0300

    Added views for CRUD operations using DRF generic views. Configured URLs for list, create, retrieve, update, and delete

--- journal/views.py
+++ journal/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import JournalEntry
+from .serializers import JournalEntrySerializer
 
-# Create your views here.
+class JournalEntryListCreateView(generics.ListCreateAPIView):
+    serializer_class = JournalEntrySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return JournalEntry.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class JournalEntryDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = JournalEntrySerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return JournalEntry.objects.filter(user=self.request.user)
