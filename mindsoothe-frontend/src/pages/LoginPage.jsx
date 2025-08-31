105e77a1a7fa467237f2c53e725940b369893611
FaizaSuraw <itssumeya@gmail.com>
2025-08-31 12:09:08 +0300

    created login page and solved issues with npm start

---
+++ mindsoothe-frontend/src/pages/LoginPage.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import api from "../api/axios";
+
+export default function Login() {
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("");
+
+    try {
+      setLoading(true);
+      const response = await api.post("auth/login/", formData);
+      localStorage.setItem("tokens", JSON.stringify(response.data));
+
+      setLoading(false);
+      navigate("/dashboard");
+    } catch (err) {
+      setLoading(false);
+      if (err.response) {
+        setError(JSON.stringify(err.response.data));
+      } else {
+        setError("An error occurred. Try again.");
+      }
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="username"
+          placeholder="Username"
+          value={formData.username}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+        <button type="submit" disabled={loading}>
+          {loading ? "Logging in..." : "Login"}
+        </button>
+      </form>
+      {error && <p style={{ color: "red" }}>{error}</p>}
+    </div>
+  );
+}
