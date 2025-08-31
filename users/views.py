504e97b7bc59dd77d22a3111021f08f0c3cc54ea
FaizaSuraw <itssumeya@gmail.com>
2025-08-31 22:06:57 +0300

    preparing for production

--- users/views.py
+++ users/views.py
@@ -1,4 +1,3 @@
-from rest_framework import status
 from rest_framework import generics, permissions
 from django.contrib.auth import get_user_model
 from .serializers import RegisterSerializer
@@ -22,20 +21,3 @@ class ProfileView(APIView):
             "username": user.username,
             "email": user.email
         })
-def put(self, request):
-        user = request.user
-        data = request.data
-
-        user.username = data.get("username", user.username)
-        user.email = data.get("email", user.email)
-
-        if password := data.get("password"):
-            user.set_password(password)
-
-        user.save()
-
-        return Response({
-            "id": user.id,
-            "username": user.username,
-            "email": user.email
-        }, status=status.HTTP_200_OK)
