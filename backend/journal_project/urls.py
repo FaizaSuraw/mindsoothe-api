62b9897ba67cc0db3c753f824de80e5bb1064edf
FaizaSuraw <itssumeya@gmail.com>
2025-09-07 18:19:48 +0300

    feat: add Welcome message to APIRoot

--- backend/journal_project/urls.py
+++ backend/journal_project/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/', include('authentication.urls')),
     path('api/', include('journal.urls')),
+    path('', lambda request: JsonResponse({"message": "Backend Working!"})),
 ]
