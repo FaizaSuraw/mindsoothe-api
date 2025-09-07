fab753ea7b5bad7417b9aced71f090e192640d49
FaizaSuraw <itssumeya@gmail.com>
2025-09-07 20:16:47 +0300

    fix: deploy gh pages

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import AppRoutes from "./routes/Routes";
+import App from "./App";
 import { AuthProvider } from "./context/AuthContext";
-import './index.css';
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="mindsoothe-api">
+    <BrowserRouter basename="/mindsoothe-api">
       <AuthProvider>
-        <AppRoutes />
+        <App />
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
